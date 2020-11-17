c06b986a1daa5e110bd0a0c08eaa62e752ad968d
kaushalkoladiya <kaushalkoladiya123@gmail.com>
2020-11-17 15:48:43 +0530

    Change Folder Structure

--- src/screens/FlatList/FlatList.tsx
+++ src/screens/FlatList/FlatList.tsx
@@ -4,92 +4,27 @@ import {
   Text,
   View,
   StyleSheet,
-  FlatList,
-  Image,
   Dimensions,
   Animated,
-  TouchableOpacity,
   Platform,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 
 // API
 import { getMovies } from "../../api/";
-// Components
-import Genres from "../../components/Genres";
-import Rating from "../../components/Rating";
 // TYPES
 import { Movie } from "../../../types";
+// Components
+import ListItem from "./ListItem";
+import Backdrop from "./Backdrop";
 
-const { width, height } = Dimensions.get("window");
-const SPACING = 10;
+const { width } = Dimensions.get("window");
 const ITEM_SIZE = Platform.OS === "ios" ? width * 0.72 : width * 0.74;
-const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
-const BACKDROP_HEIGHT = height * 0.65;
 
 const Loading = () => (
   <View style={styles.loadingContainer}>
     <Text style={styles.paragraph}>Loading...</Text>
   </View>
 );
-
-const Backdrop = ({
-  movies,
-  scrollX,
-}: {
-  movies: Array<Movie>;
-  scrollX: any;
-}) => {
-  return (
-    <View style={{ height: BACKDROP_HEIGHT, width, position: "absolute" }}>
-      <FlatList
-        data={movies.reverse()}
-        keyExtractor={(item) => item.key + "-backdrop"}
-        removeClippedSubviews={false}
-        contentContainerStyle={{ width, height: BACKDROP_HEIGHT }}
-        renderItem={({ item, index }) => {
-          if (!item.backdrop) {
-            return null;
-          }
-          const translateX = scrollX.interpolate({
-            inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
-            outputRange: [0, width],
-            // extrapolate:'clamp'
-          });
-          return (
-            <Animated.View
-              removeClippedSubviews={false}
-              style={{
-                position: "absolute",
-                width: translateX,
-                height,
-                overflow: "hidden",
-              }}
-            >
-              <Image
-                source={{ uri: item.backdrop }}
-                style={{
-                  width,
-                  height: BACKDROP_HEIGHT,
-                  position: "absolute",
-                }}
-              />
-            </Animated.View>
-          );
-        }}
-      />
-      <LinearGradient
-        colors={["rgba(0, 0, 0, 0)", "white"]}
-        style={{
-          height: BACKDROP_HEIGHT,
-          width,
-          position: "absolute",
-          bottom: 0,
-        }}
-      />
-    </View>
-  );
-};
 
 export default function App() {
   const [movies, setMovies] = React.useState<Movie[] | []>([]);
@@ -134,51 +69,9 @@ export default function App() {
           { useNativeDriver: false }
         )}
         scrollEventThrottle={16}
-        renderItem={({ item, index }: { item: Movie; index: number }) => {
-          if (!item.poster) {
-            return <View style={{ width: EMPTY_ITEM_SIZE }} />;
-          }
-
-          const inputRange = [
-            (index - 2) * ITEM_SIZE,
-            (index - 1) * ITEM_SIZE,
-            index * ITEM_SIZE,
-          ];
-
-          const translateY = scrollX.interpolate({
-            inputRange,
-            outputRange: [100, 50, 100],
-            extrapolate: "clamp",
-          });
-
-          return (
-            <View style={{ width: ITEM_SIZE }}>
-              <Animated.View
-                style={{
-                  marginHorizontal: SPACING,
-                  padding: SPACING * 2,
-                  alignItems: "center",
-                  transform: [{ translateY }],
-                  backgroundColor: "white",
-                  borderRadius: 34,
-                }}
-              >
-                <Image
-                  source={{ uri: item.poster }}
-                  style={styles.posterImage}
-                />
-                <Text style={{ fontSize: 24 }} numberOfLines={1}>
-                  {item.title}
-                </Text>
-                <Rating rating={item.rating} />
-                <Genres genres={item.genres} />
-                <Text style={{ fontSize: 12 }} numberOfLines={3}>
-                  {item.description}
-                </Text>
-              </Animated.View>
-            </View>
-          );
-        }}
+        renderItem={({ item, index }: { item: Movie; index: number }) => (
+          <ListItem item={item} index={index} key={index} scrollX={scrollX} />
+        )}
       />
     </View>
   );
@@ -198,13 +91,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
-  },
-  posterImage: {
-    width: "100%",
-    height: ITEM_SIZE * 1.2,
-    resizeMode: "cover",
-    borderRadius: 24,
-    margin: 0,
-    marginBottom: 10,
   },
 });
